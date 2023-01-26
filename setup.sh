92f6ea205fb62d450a3671d8767d4bfba179997e
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-01-26 23:16:01 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -166,57 +166,28 @@ echo ""
 #wget -q https://raw.githubusercontent.com/Tarap-Kuhing/sc/main/tools.sh;chmod +x tools.sh;./tools.sh
 #rm tools.sh
 clear
-echo ""
-# ==========================================
-# Getting
-fi
-clear
-apt install jq curl -y
-domain=hendra93.my.id
-echo -e  "|\033[1;31m============================================\033[0m|"
+echo -e  "|\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m|"
 echo -e  "|\033[0;33m    TERIMA KASIH SUDAH MENGGUNAKAN SCRIPT   \033[0;33m|"
-echo -e  "|\033[0;33m    MOD DARI SAYA BY TARAP KUHING           \033[0;33m|"
-echo -e  "|\033[0;33m          ADA PERTANYAAN CHAT               \033[0;33m|"
-echo -e  "|\033[0;33m    WA :     085754292950                   \033[0;33m|"
-echo -e  "|\033[1;31m============================================\033[0m|"
-read -rp " TEKAN ENTER UNTUK MELANJUTKAN "
-dns=$(</dev/urandom tr -dc a-z0-9 | head -c6)
-dns=${sub}.hendra93.my.id
-CF_ID=merahjambo@gmail.com
-CF_KEY=86431de017f7bf317c3960061da2f87c8effb
-set -euo pipefail
-IP=$(wget -qO- ipinfo.io/ip);
-echo "Updating DNS for ${dns}..."
-ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${domain}&status=active" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" | jq -r .result[0].id)
-
-RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${dns}" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" | jq -r .result[0].id)
-
-if [[ "${#RECORD}" -le 10 ]]; then
-     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" \
-     --data '{"type":"A","name":"'${dns}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
-fi
-
-RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" \
-     --data '{"type":"A","name":"'${dns}'","content":"'${IP}'","ttl":120,"proxied":false}')
-     echo "$dns" > /root/scdomain
+echo -e  "|\033[0;33m         DARI SAYA BY TARAP KUHING          \033[0;33m|"
+echo -e  "|\033[0;33m        TAMBAHKAN DOMAIN KAMU  DISINI       \033[0;33m|"
+echo -e  "|\033[0;33m          KALAU TIDAK PUNYA DOMAIN          \033[0;33m|"
+echo -e  "|\033[0;33m          CHAT DI WA SAYA DULU OK....       \033[0;33m|"
+echo -e  "|\033[0;33m          https://wa.me/085754292950        \033[0;33m|"
+echo -e  "|\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m|"
+echo " "
+read -rp "Masukan domain kamu Disini : " -e dns
+    if [ -z $dns ]; then
+        echo -e "
+        Nothing input for domain!
+        Then a random domain will be created"
+    else
+        echo "$dns" > /root/scdomain
 	echo "$dns" > /etc/xray/scdomain
 	echo "$dns" > /etc/xray/domain
 	echo "$dns" > /etc/v2ray/domain
 	echo $dns > /root/domain
         echo "IP=$dns" > /var/lib/ipvps.conf
-       fi
+    fi
 
 #install ssh ovpn
 echo -e "${tyblue}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
