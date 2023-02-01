587d0d2ec03a4cdba1752f4c3a4b59bb3bb86ab4
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-02-01 23:53:54 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -294,7 +294,7 @@ if [ ! -f "/etc/log-create-user.log" ]; then
 echo "Log All Account " > /etc/log-create-user.log
 fi
 history -c
-serverV=$( curl -sS https://raw.githubusercontent.com/Tarap-Kuhing/Profile/main/Profile/permission/versi  )
+serverV=$( curl -sS https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/versi  )
 echo $serverV > /opt/.ver
 aureb=$(cat /home/re_otm)
 b=11
