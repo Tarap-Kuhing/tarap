c1041d2fe8bc30dc9cb889cb6dc0eaef0b7ebb93
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-02-01 09:44:33 +0800

    Update m-domain.sh

--- menu/m-domain.sh
+++ menu/m-domain.sh
@@ -39,7 +39,7 @@ read -p " Select menu : " opt
 echo -e ""
 case $opt in
 1) clear ; add-host ;;
-2) clear ; certv2ray ;;
+2) clear ; certxray ;;
 0) clear ; menu ;;
 x) exit ;;
 *) echo "Anda salah tekan" ; sleep 1 ; m-domain ;;
