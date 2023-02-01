2dddfbb73fb8600c7ced744a45f6f08582fb86d6
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-02-01 19:43:33 +0800

    Update update.sh

--- menu/update.sh
+++ menu/update.sh
@@ -4,27 +4,24 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
 echo -e " [INFO] Downloading Update File"
 sleep 2
-wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/MENU/menu.sh" && chmod +x /usr/bin/menu
-wget -q -O /usr/bin/menu-ss "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/MENU/menu-ss.sh" && chmod +x /usr/bin/menu-ss
-wget -q -O /usr/bin/menu-vmess "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/MENU/menu-vmess.sh" && chmod +x /usr/bin/menu-vmess
-wget -q -O /usr/bin/menu-vless "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/MENU/menu-vless.sh" && chmod +x /usr/bin/menu-vless
-wget -q -O /usr/bin/menu-trojan "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/MENU/menu-trojan.sh" && chmod +x /usr/bin/menu-trojan
-wget -q -O /usr/bin/menu-bot "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/MENU/menu-bot.sh" && chmod +x /usr/bin/menu-bot
-wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/MENU/menu-ssh.sh" && chmod +x /usr/bin/menu-ssh
-wget -q -O /usr/bin/menu-set "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/MENU/menu-set.sh" && chmod +x /usr/bin/menu-set
-wget -q -O /usr/bin/menu-theme "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/MENU/menu-theme.sh" && chmod +x /usr/bin/menu-theme
-wget -q -O /usr/bin/menu-backup "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/MENU/menu-backup.sh" && chmod +x /usr/bin/menu-backup
-wget -q -O /usr/bin/menu-ip "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/MENU/menu-ip.sh" && chmod +x /usr/bin/menu-ip
-wget -q -O /usr/bin/menu-tor "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/MENU/menu-tor.sh" && chmod +x /usr/bin/menu-tor
-wget -q -O /usr/bin/autoboot "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/MENU/autoboot.sh" && chmod +x /usr/bin/autoboot
-wget -q -O /usr/bin/menu-tcp "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/MENU/menu-tcp.sh" && chmod +x /usr/bin/menu-tcp
-wget -q -O /usr/bin/rebootvps "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/MENU/rebootvps.sh" && chmod +x /usr/bin/rebootvps
-wget -q -O /usr/bin/menu-dns "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/MENU/menu-dns.sh" && chmod +x /usr/bin/menu-dns
-wget -q -O /usr/bin/info "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/MENU/info.sh" && chmod +x /usr/bin/info
-wget -q -O /usr/bin/mspeed "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/MENU/menu-speedtest.sh" && chmod +x /usr/bin/mspeed
-wget -q -O /usr/bin/mbandwith "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/MENU/menu-bandwith.sh" && chmod +x /usr/bin/mbandwith
-wget -q -O /usr/bin/restart "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/MENU/restart.sh" && chmod +x /usr/bin/restart
-wget -q -O /usr/bin/update "https://raw.githubusercontent.com/Tarap-Kuhing/tarong/main/tarong/MENU/update.sh" && chmod +x /usr/bin/update
-echo -e " [INFO] Update Successfully"
+wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/menu.sh" && chmod +x /usr/bin/menu
+wget -q -O /usr/bin/m-vmess "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-vmess.sh" && chmod +x /usr/bin/m-vmess
+wget -q -O /usr/bin/m-vless "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-vless.sh" && chmod +x /usr/bin/m-vless
+wget -q -O /usr/bin/m-trojan "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-trojan.sh" && chmod +x /usr/bin/m-trojan
+wget -q -O /usr/bin/m-system "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-system.sh" && chmod +x /usr/bin/m-system
+wget -q -O /usr/bin/m-sshovpn "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-sshovpn.sh" && chmod +x /usr/bin/m-sshovpn
+wget -q -O /usr/bin/m-ssws "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-ssws.sh" && chmod +x /usr/bin/m-ssws
+wget -q -O /usr/bin/m-webmin "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-webmin.sh" && chmod +x /usr/bin/m-webmin
+wget -q -O /usr/bin/running "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/running.sh" && chmod +x /usr/bin/running
+wget -q -O /usr/bin/bw "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/bw.sh" && chmod +x /usr/bin/bw
+wget -q -O /usr/bin/m-tcp "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/tcp.sh" && chmod +x /usr/bin/m-tcp
+wget -q -O /usr/bin/auto-reboot "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/auto-reboot.sh" && chmod +x /usr/bin/auto-reboot
+wget -q -O /usr/bin/clearcache "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/clearcache.sh" && chmod +x /usr/bin/clearcache
+wget -q -O /usr/bin/restart "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/restart.sh" && chmod +x /usr/bin/restart
+wget -q -O /usr/bin/update "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-update.sh" && chmod +x /usr/bin/m-update
+wget -q -O /usr/bin/backup "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/ssh/backup.sh" && chmod +x /usr/bin/backup
+wget -q -O /usr/bin/autobackup "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/ssh/autobackup.sh" && chmod +x /usr/bin/autobackup
+wget -q -O /usr/bin/limitspeed "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/ssh/limitspeed.sh" && chmod +x /usr/bin/limitspeed
+wget -q -O /usr/bin/m-theme "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-theme.sh" && chmod +x /usr/bin/m-theme
 sleep 2
 exit
