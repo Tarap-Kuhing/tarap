e4ddc9610c7be641ed81f30fc714f72ccb3a74d6
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-02-03 00:56:33 +0800

    Update backup.sh

--- ssh/backup.sh
+++ ssh/backup.sh
@@ -1,96 +1,48 @@
 #!/bin/bash
-MYIP=dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-#########################
-
-BURIQ () {
-    curl -sS https://raw.githubusercontent.com/Tarap-Kuhing/Profile/main/Profile/permission/ip > /root/tmp
-    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
-    for user in "${data[@]}"
-    do
-    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
-    d1=(`date -d "$exp" +%s`)
-    d2=(`date -d "$biji" +%s`)
-    exp2=$(( (d1 - d2) / 86400 ))
-    if [[ "$exp2" -le "0" ]]; then
-    echo $user > /etc/.$user.ini
-    else
-    rm -f /etc/.$user.ini > /dev/null 2>&1
-    fi
-    done
-    rm -f /root/tmp
-}
-
-MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/Tarap-Kuhing/Profile/main/Profile/permission/ip | grep $MYIP | awk '{print $2}')
-echo $Name > /usr/local/etc/.$Name.ini
-CekOne=$(cat /usr/local/etc/.$Name.ini)
-
-Bloman () {
-if [ -f "/etc/.$Name.ini" ]; then
-CekTwo=$(cat /etc/.$Name.ini)
-    if [ "$CekOne" = "$CekTwo" ]; then
-        res="Expired"
-    fi
-else
-res="Permission Accepted..."
-fi
-}
-
-PERMISSION () {
-    MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/Tarap-Kuhing/Profile/main/Profile/permission/ip | awk '{print $4}' | grep $MYIP)
-    if [ "$MYIP" = "$IZIN" ]; then
-    Bloman
-    else
-    res="Permission Denied!"
-    fi
-    BURIQ
-}
-
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-PERMISSION
-if [ -f /home/needupdate ]; then
-red "Your script need to update first !"
-exit 0
-elif [ "$res" = "Permission Accepted..." ]; then
-echo -ne
-else
-red "Permission Denied!"
-exit 0
-fi
-echo -e "\e[32mloading...\e[0m"
+# Mod By TARAP KUHING
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
 clear
-IP=$(wget -qO- icanhazip.com);
+IP=$(wget -qO- ipinfo.io/ip);
 date=$(date +"%Y-%m-%d")
 clear
-echo " Enter Your Email To Receive Message"
-read -rp " Email: " -e email
-sleep 1
-echo Directory Created
+email=$(cat /home/email)
+if [[ "$email" = "" ]]; then
+echo "Masukkan Email Untuk Menerima Backup"
+read -rp "Email : " -e email
+cat <<EOF>>/home/email
+$email
+EOF
+fi
+clear
+echo "Mohon Menunggu , Proses Backup sedang berlangsung !!"
 rm -rf /root/backup
 mkdir /root/backup
-sleep 1
-echo Start Backup
-clear
-cp -r /root/.acme.sh /root/backup/ &> /dev/null
-cp /etc/passwd /root/backup/ &> /dev/null
-cp /etc/group /root/backup/ &> /dev/null
-cp /etc/shadow /root/backup/ &> /dev/null
-cp /etc/gshadow /root/backup/ &> /dev/null
-cp /etc/ppp/chap-secrets /root/backup/chap-secrets &> /dev/null
-cp /etc/shadowsocks-libev/akun.conf /root/backup/ss.conf &> /dev/null
-cp -r /var/lib /root/backup &> /dev/null
-cp -r /etc/v2ray /root/backup/v2ray &> /dev/null
-cp -r /etc/xray /root/backup/xray &> /dev/null
-cp -r /usr/local/shadowsocksr/ /root/backup/shadowsocksr &> /dev/null
-cp -r /home/vps/public_html /root/backup/public_html &> /dev/null
-cp -r /etc/cron.d /root/backup/cron.d &> /dev/null
-cp /etc/crontab /root/backup/crontab &> /dev/null
+cp /etc/passwd backup/
+cp /etc/group backup/
+cp /etc/shadow backup/
+cp /etc/gshadow backup/
+cp -r /etc/wireguard backup/wireguard
+cp /etc/ppp/chap-secrets backup/chap-secrets
+cp /etc/ipsec.d/passwd backup/passwd1
+cp /etc/shadowsocks-libev/akun.conf backup/ss.conf
+cp -r /var/lib/ backup
+cp -r /home/sstp backup/sstp
+cp -r /etc/xray backup/xray
+cp -r /etc/v2ray backup/v2ray
+cp -r /etc/trojan-go backup/trojan-go
+cp -r /usr/local/shadowsocksr/ backup/shadowsocksr
+cp -r /home/vps/public_html backup/public_html
 cd /root
 zip -r $IP-$date.zip backup > /dev/null 2>&1
 rclone copy /root/$IP-$date.zip dr:backup/
@@ -108,17 +60,12 @@ Tanggal       : $date
 rm -rf /root/backup
 rm -r /root/$IP-$date.zip
 clear
-echo -e "
+echo -e ""
 Detail Backup
 ==================================
 IP VPS        : $IP
 Link Backup   : $link
 Tanggal       : $date
 ==================================
-echo -e ""
-If you want to restore data, please enter the link above.
-Thank You For Using Our Services" | mail -s "Backup Data" $email
-rm -rf /root/backup
-rm -r /root/$IP-$date.zip
-echo "Done"
-echo "Please Check Your Email"
+"
+echo "Silahkan cek Kotak Masuk $email"
