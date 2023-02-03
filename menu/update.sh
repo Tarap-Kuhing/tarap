fca4fab7dd360e881a2929ab4ae6cace14fc8cea
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-02-03 11:50:01 +0800

    Update update.sh

--- menu/update.sh
+++ menu/update.sh
@@ -58,7 +58,6 @@ wget -q -O /usr/bin/cek-vless "https://raw.githubusercontent.com/Tarap-Kuhing/ta
 wget -q -O /usr/bin/cek-tr "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/xray/cek-tr.sh" && chmod +x /usr/bin/cek-tr
 wget -q -O /usr/bin/cek-ssws "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/xray/cek-ssws.sh" && chmod +x /usr/bin/cek-ssws
 wget -q -O /usr/bin/certv2ray "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/xray/certv2ray.sh" && chmod +x /usr/bin/certv2ray
-wget -q -O /usr/bin/m-set "https://raw.githubusercontent.com/Tarap-Kuhing/tarap/main/menu/m-set.sh" && chmod +x /usr/bin/m-set
 echo -e " [INFO] Update Successfully"
 sleep 2
 exit
