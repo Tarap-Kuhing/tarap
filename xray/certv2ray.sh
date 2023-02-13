d21fceff8d7377d97a9b567f13c5adc928e54680
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-02-13 13:49:48 +0800

    Update certv2ray.sh

--- xray/certv2ray.sh
+++ xray/certv2ray.sh
@@ -73,7 +73,7 @@ cekray=`cat /root/log-install.txt | grep -ow "XRAY" | sort | uniq`
 if [ "$cekray" = "XRAY" ]; then
 domainlama=`cat /etc/xray/domain`
 else
-domainlama=`cat /etc/v2ray/domain`
+domainlama=`cat /etc/xray/domain`
 fi
 
 clear
@@ -104,7 +104,6 @@ sleep 2
 echo -e "[ ${green}INFO${NC} ] Starting service $Cek "
 sleep 2
 echo $domain > /etc/xray/domain
-echo $domain > /etc/v2ray/domain
 systemctl restart $Cek
 systemctl restart nginx
 echo -e "[ ${green}INFO${NC} ] All finished... "
