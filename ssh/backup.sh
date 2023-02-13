6953b0223b979f9921a23277c8e4e468e0f14307
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-02-13 20:16:08 +0800

    Update backup.sh

--- ssh/backup.sh
+++ ssh/backup.sh
@@ -13,32 +13,28 @@ LIGHT='\033[0;37m'
 # ==========================================
 # Getting
 clear
-IP=$(wget -qO- ipinfo.io/ip);
+echo -e "\e[32mloading...\e[0m"
+clear
+IP=$(wget -qO- icanhazip.com);
 date=$(date +"%Y-%m-%d")
-clear
-rm -rf /root/email
-mkdir /root/email
 email=$(cat /home/email)
-if [[ "$email" = "" ]]; then
-echo "Masukkan Email Untuk Menerima Backup"
-read -rp "Email : " -e email
-cat <<EOF>>/home/email
-$email
-EOF
-fi
-clear
-echo "Mohon Menunggu , Proses Backup sedang berlangsung !!"
-rm -rf /root/backup
+echo Directory Created
 mkdir /root/backup
+sleep 1
+echo Start Backup
+clear
 cp /etc/passwd backup/
 cp /etc/group backup/
 cp /etc/shadow backup/
 cp /etc/gshadow backup/
+cp -r /etc/wireguard backup/wireguard
 cp /etc/shadowsocks-libev/akun.conf backup/ss.conf
 cp -r /var/lib/ backup
-cp -r /etc /usr/local/xray
-cp -r /usr/local/xray
+cp -r /usr/local/etc/xray
+cp -r /usr/local/etc/xray backup/xray
+cp -r /etc/trojan-go backup/trojan-go
 cp -r /usr/local/shadowsocksr/ backup/shadowsocksr
+cp /etc/crontab backup/crontab
 cp -r /home/vps/public_html backup/public_html
 cd /root
 zip -r $IP-$date.zip backup > /dev/null 2>&1
@@ -46,23 +42,15 @@ rclone copy /root/$IP-$date.zip dr:backup/
 url=$(rclone link dr:backup/$IP-$date.zip)
 id=(`echo $url | grep '^https' | cut -d'=' -f2`)
 link="https://drive.google.com/u/4/uc?id=${id}&export=download"
-echo -e "
-Detail Backup
-==================================
-IP VPS        : $IP
-Link Backup   : $link
-Tanggal       : $date
-==================================
-" | mail -s "Backup Data" $email
+echo -e "The following is a link to your vps data backup file.
+
+Your VPS IP $IP
+
+$link
+
+If you want to restore data, please enter the link above.
+
+Thank You For Using Our Services" | mail -s "Backup Data" $email
 rm -rf /root/backup
 rm -r /root/$IP-$date.zip
-clear
-echo -e "
-Detail Backup
-==================================
-IP VPS        : $IP
-Link Backup   : $link
-Tanggal       : $date
-==================================
-"
-echo "Silahkan cek Kotak Masuk $email"
+echo Done
