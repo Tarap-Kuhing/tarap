a9dabf7d5eb027db1b483ebec0599cb6bb0e9a6c
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-02-19 10:40:13 +0700

    Create path.sh

---
+++ xray/path.sh
@@ -0,0 +1,53 @@
+cat >/etc/xray/path <<-EOF
+$domain:443
+{
+    tls merahjambo@gmail.com
+    encode gzip
+
+
+
+    handle_path /vless {
+        reverse_proxy localhost:14016
+
+    }
+    import vmess
+    handle_path /vmess {
+        reverse_proxy localhost:23456
+    }
+
+    handle_path /trojan-ws {
+        reverse_proxy localhost:25432
+    }
+
+
+    handle_path /ss-ws {
+        reverse_proxy localhost:30300
+    }
+
+    handle {
+        reverse_proxy https://$domain {
+            trusted_proxies 0.0.0.0/0
+            header_up Host {upstream_hostport}
+        }
+    }
+}
+EOF
+
+
+cat >/etc/xray/vmess <<-EOF
+
+@ws_path {
+path /worryfree
+path /vmess
+path /xray
+path /*
+
+}
+
+
+handle @ws_path {
+    uri path_regexp /.* /
+    reverse_proxy localhost:23456
+}
+
+EOF
