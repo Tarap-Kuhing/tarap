79a5eaec484d4ed0dde84ed113ec2284267ed655
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-02-24 20:22:42 +0800

    Update m-system.sh

--- menu/m-system.sh
+++ menu/m-system.sh
@@ -41,15 +41,15 @@ echo -e "  ${WH}[${COLOR1}01${WH}]${NC} ${COLOR1}• ${WH}CHANGE DOMAIN   ${WH} 
 echo -e " $COLOR1 $NC                                              ${NC} $COLOR1 $NC"
 echo -e "  ${WH}[${COLOR1}02${WH}]${NC} ${COLOR1}• ${WH}SPEEDTEST   ${WH}     ${WH}[${COLOR1}09${WH}]${NC} ${COLOR1}• ${WH}LIMIT SPEED SERVER ${WH} $COLOR1 $NC"
 echo -e " $COLOR1 $NC                                              ${NC} $COLOR1 $NC"
-echo -e "  ${WH}[${COLOR1}03${WH}]${NC} ${COLOR1}• ${WH}AUTO REBOOT   ${WH}   ${WH}[${COLOR1}10${WH}]${NC} ${COLOR1}• ${WH}AUTO BACKUP DATA  ${WH}  $COLOR1 $NC"
+echo -e "  ${WH}[${COLOR1}03${WH}]${NC} ${COLOR1}• ${WH}AUTO REBOOT   ${WH}   ${WH}[${COLOR1}10${WH}]${NC} ${COLOR1}• ${WH}SET BANNER  ${WH}        $COLOR1 $NC"
 echo -e " $COLOR1 $NC                                              ${NC} $COLOR1 $NC"
-echo -e "  ${WH}[${COLOR1}04${WH}]${NC} ${COLOR1}• ${WH}RESTART VPS   ${WH}   ${WH}[${COLOR1}11${WH}]${NC} ${COLOR1}• ${WH}BACKUP DATA ${WH}        $COLOR1 $NC"
+echo -e "  ${WH}[${COLOR1}04${WH}]${NC} ${COLOR1}• ${WH}RESTART VPS   ${WH}   ${WH}[${COLOR1}11${WH}]${NC} ${COLOR1}• ${WH}BACKUP MENU ${WH}        $COLOR1 $NC"
 echo -e " $COLOR1 $NC                                              ${NC} $COLOR1 $NC"
-echo -e "  ${WH}[${COLOR1}05${WH}]${NC} ${COLOR1}• ${WH}CEK BANDWITH  ${WH}   ${WH}[${COLOR1}12${WH}]${NC} ${COLOR1}• ${WH}RESTORE DATA ${WH}       $COLOR1 $NC"
+echo -e "  ${WH}[${COLOR1}05${WH}]${NC} ${COLOR1}• ${WH}CEK BANDWITH  ${WH}   ${WH}[${COLOR1}12${WH}]${NC} ${COLOR1}• ${WH}DNS ${WH}                $COLOR1 $NC"
 echo -e " $COLOR1 $NC                                              ${NC} $COLOR1 $NC"
-echo -e "  ${WH}[${COLOR1}06${WH}]${NC} ${COLOR1}• ${WH}INSTALL TCP BBR ${WH} ${WH}[${COLOR1}13${WH}]${NC} ${COLOR1}• ${WH}SET BANNER ${WH}         $COLOR1 $NC"
+echo -e "  ${WH}[${COLOR1}06${WH}]${NC} ${COLOR1}• ${WH}INSTALL TCP BBR ${WH}                                $COLOR1 $NC"
 echo -e " $COLOR1 $NC                                              ${NC} $COLOR1 $NC"
-echo -e "  ${WH}[${COLOR1}07${WH}]${NC} ${COLOR1}• ${WH}IP         ${WH}      ${WH}[${COLOR1}14${WH}]${NC} ${COLOR1}• ${WH}DNS ${WH}         $COLOR1 $NC"
+echo -e "  ${WH}[${COLOR1}07${WH}]${NC} ${COLOR1}• ${WH}IP  ${WH}                                            $COLOR1 $NC"
 echo -e "$COLOR1 ${NC}"
 echo -e "  ${WH}[${COLOR1}00${WH}]${NC} ${COLOR1}• ${WH}GO BACK $NC"
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
@@ -65,11 +65,9 @@ case $opt in
 07 |7) clear ; m-ip ; exit ;;
 08 |8) clear ; webmin ; exit ;;
 09 |9) clear ; limitspeed ; exit ;;
-10 |10) clear ; autobackup ; exit ;;
-11 |11) clear ; backup ; exit ;;
-12 |12) clear ; restore ; exit ;;
-13 |13) clear ; nano /etc/issue.net ;;
-14 |14) clear ; m-dns ; exit ;;
+10 |10) clear ; nano /etc/issue.net ;;
+11 |11) clear ; m-backup ; exit ;;
+12 |12) clear ; m-dns ; exit ;;
 00 |0) clear ; menu ; exit ;;
 *) echo -e "" ; echo "Anda salah tekan" ; sleep 1 ; m-system ;;
 esac
