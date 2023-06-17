8f478adbe31cc07737a65b04251467d4404616cf
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-06-17 23:05:21 +0800

    @Baung

--- Tarap-Kuhing/limit.sh
+++ Tarap-Kuhing/limit.sh
@@ -3,10 +3,10 @@ wget -q -O /etc/systemd/system/limitvmess.service "${REPO}Tarap-Kuhing/limitvmes
 wget -q -O /etc/systemd/system/limitvless.service "${REPO}Tarap-Kuhing/limitvless.service" && chmod +x limitvless.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limittrojan.service "${REPO}Tarap-Kuhing/limittrojan.service" && chmod +x limittrojan.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limitshadowsocks.service "${REPO}Tarap-Kuhing/limitshadowsocks.service" && chmod +x limitshadowsocks.service >/dev/null 2>&1
-wget -q -O /etc/xray/limit.vmess "${REPO}bin/vmess" >/dev/null 2>&1
-wget -q -O /etc/xray/limit.vless "${REPO}bin/vless" >/dev/null 2>&1
-wget -q -O /etc/xray/limit.trojan "${REPO}bin/trojan" >/dev/null 2>&1
-wget -q -O /etc/xray/limit.shadowsocks "${REPO}bin/shadowsocks" >/dev/null 2>&1
+wget -q -O /etc/xray/limit.vmess "${REPO}Tarap-Kuhing/vmess" >/dev/null 2>&1
+wget -q -O /etc/xray/limit.vless "${REPO}Tarap-Kuhing/vless" >/dev/null 2>&1
+wget -q -O /etc/xray/limit.trojan "${REPO}Tarap-Kuhing/trojan" >/dev/null 2>&1
+wget -q -O /etc/xray/limit.shadowsocks "${REPO}Tarap-Kuhing/shadowsocks" >/dev/null 2>&1
 chmod +x /etc/xray/limit.vmess
 chmod +x /etc/xray/limit.vless
 chmod +x /etc/xray/limit.trojan
